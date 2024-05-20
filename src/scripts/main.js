7094cb210c6da0559487e0a62431f345ba27edb2
unknown <roman.kovalchuk220702@gmail.com>
2024-05-20 19:08:41 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const switcher = document.querySelector('.swing-trade__switcher');
+const priceCheap = document.querySelectorAll('.swing-trade__price--cheap');
+const priceExpensive = document.querySelectorAll('.swing-trade__price--expensive');
+const month = document.querySelector('.swing-trade__switcher-text--month');
+const year = document.querySelector('.swing-trade__switcher-text--year');
+console.log(priceCheap.textContent)
+
+switcher.addEventListener('click', () => {
+  if (switcher.classList.contains('swing-trade__switcher--work--on')) {
+    switcher.classList.remove('swing-trade__switcher--work--on');
+    year.classList.remove('swing-trade__switcher-text--color--white');
+    month.classList.add('swing-trade__switcher-text--color--white');
+    priceCheap.textContent = '$150';
+    priceExpensive.textContent = '$170';
+  } else {
+    switcher.classList.add('swing-trade__switcher--work--on');
+    priceCheap.textContent = '$199';
+    priceExpensive.textContent = '$300';
+    year.classList.add('swing-trade__switcher-text--color--white');
+    month.classList.remove('swing-trade__switcher-text--color--white');
+  }
+});
+
+
